428db84bd69d98239ec95a38d3c64e263d62b73c
manavkhadka0 <manavkhadka0@gmail.com>
2021-06-25 18:04:47 +0545

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "very-dark-cyan": "hsl(179, 100%, 13%)",
+        "dark-cyan": "hsl(184, 100%, 22%)",
+        "bright-orange": "hsl(31, 77%, 52%)",
+        "very-light-grey": "hsl(0, 0%, 95%)",
+        "transparent-white": "hsla(0, 0%, 100%, 0.75)",
+      },
+    },
+    fontFamily: {
+      "Lexend": ['Lexend Deca', "sans-serif"],
+      "Big": ['Big Shoulders Display', "sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "9rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
